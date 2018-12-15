78042aca530ce30c559c9caacbb16b89efe5bc62
Samaneh Nazari <nazari.samaneh@gmail.com>
2018-12-15 16:41:25 +0330

    init component

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Search extends React.Component {
+    render() {
+        return (
+            <div>This is so Fancy!</div>
+    );
+    }
+}
+export default Search;
